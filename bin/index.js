644f6ec8fb3d7b472ad32c44b69474c99f19629e
Dammy8611 <damy84511@gmail.com>
2023-11-14 16:16:46 +0100

    secon commit

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,26 @@
 #! /usr/bin/env node
-console.log("Hello World This is the first program!");
+
+const yargs = require("yargs");
+
+yargs
+  .command({
+    command: "greet",
+    describe: "Greet the user",
+    handler: () => {
+      console.log("Hello, user!");
+    },
+  })
+
+  .command({
+    command: "*",
+    describe: "Command not found",
+    handler: (argv) => {
+      if (argv._.length === 0) {
+        console.log(argv);
+        return;
+      }
+      console.log(`Command not found: ${argv._.join(" ")}`);
+    },
+  })
+  .demandCommand(1, "Please provide a command.")
+  .parse();
